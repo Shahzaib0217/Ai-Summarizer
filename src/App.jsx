a50737ac33a7904eac2a2ce3f79572356b6f7a81
shahzaib <f201079@cfd.nu.edu.pk>
2023-07-12 18:32:19 +0500

    Basic files and vite, tailwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Hero from "./components/Hero";
+import Demmo from "./components/Demo";
+import '../'
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
