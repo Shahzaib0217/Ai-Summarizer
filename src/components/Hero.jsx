d4c345582a18f81ff4688a94a44d4ff979be2480
shahzaib <f201079@cfd.nu.edu.pk>
2023-07-13 18:31:15 +0500

    App done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full">
+        <img src={logo} alt="sumTool_logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => {
+            window.open("https://github.com/Shahzaib0217/Ai-Summarizer");
+          }}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with
+        {/* only breaks on large devices */}
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAi GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
 
-export default Hero
+export default Hero;
